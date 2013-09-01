b3a2c44906a156e0cb26f2e37fae43e20eccec8a
Dariusz Walczak <walczak.darek@gmail.com>
2013-09-01 14:38:37 +0200

    PanZooBar

--- maps/templates/map.js
+++ maps/templates/map.js
@@ -209,7 +209,7 @@ gnt.maps.create_map = function (map_div, callback_function) {
     //make sure mapOptions controls are set correct
     mapOptions.controls = [new OpenLayers.Control.Navigation(),
                            new OpenLayers.Control.Attribution(),
-                           new OpenLayers.Control.PanZoom()
+                           new OpenLayers.Control.PanZoomBar()
                            ];
     {% if layer_data|length > 1 %}
         mapOptions.controls.push(new OpenLayers.Control.LayerSwitcher());
